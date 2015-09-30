b2c99a5895a8d44e3cc8220a10ec112faa6d93b7
somename <someemail>
2015-09-30 15:35:54 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -4,8 +4,10 @@ public class SudokuVerifier {
 
 	public static int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		return 1;
-	}
+		int checksum = 0;
+		if (verifyStringLength()) checksum = 1;
+		return checksum;
+			}
 
 	public static boolean verifyStringLength() {
 		// TODO Auto-generated method stub
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,22 +2,30 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 	
 	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 
+	private SudokuVerifier verifier;
+	
+	@Before
+	public void setup(){
+		verifier = new SudokuVerifier();
+	}
+	
 	@Test
 	public void testverify() {
 		
-		assertEquals (1, SudokuVerifier.verify(correctSolution));
+		assertEquals (1, verifier.verify(correctSolution));
 	}
 	
 	@Test
 	public void testVerifyStringLength_correctSolution_returnTrue(){
 		
-		assertEquals (true, SudokuVerifier.verifyStringLength());
+		assertEquals (true, verifier.verifyStringLength());
 		
 	}
 
