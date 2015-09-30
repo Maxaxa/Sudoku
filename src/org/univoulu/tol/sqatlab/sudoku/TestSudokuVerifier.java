51f31d79734edb24fa76641803e7eac521fb2e2e
somename <someemail>
2015-09-30 15:24:25 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,11 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	
+	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 
 	@Test
 	public void testverify() {
-		String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
 		assertEquals (1, SudokuVerifier.verify(correctSolution));
+	}
+	
+	@Test
+	public void testVerifyStringLength_correctSolution_returnTrue(){
+		
+		assertEquals (true, SudokuVerifier.verifyStringLength());
+		
 	}
 
 }
