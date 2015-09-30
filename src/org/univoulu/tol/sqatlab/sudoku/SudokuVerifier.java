00633934bfb4fd58b0dd516577ad6d1c0b314a2e
somename <someemail>
2015-09-30 15:37:38 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,15 +2,20 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public static int verify(String candidateSolution) {
+	public SudokuVerifier(){
+		
+	}
+	
+	
+	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
 		int checksum = 0;
 		if (verifyStringLength()) checksum = 1;
 		return checksum;
 			}
 
-	public static boolean verifyStringLength() {
-		// TODO Auto-generated method stub
+	public boolean verifyStringLength() {
+		// returns 1 if length is correct
 		return true;
 	}
 }
