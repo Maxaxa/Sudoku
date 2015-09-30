7a6ae4fc50a1cfe970092f313aa13ed30f6dc031
somename <someemail>
2015-09-30 15:47:21 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,6 +9,8 @@ public class TestSudokuVerifier {
 	
 	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 
+	String incorrectSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
 	private SudokuVerifier verifier;
 	
 	@Before
@@ -17,7 +19,7 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void testverify() {
+	public void testverify_correctSolution_return1() {
 		
 		assertEquals (1, verifier.verify(correctSolution));
 	}
@@ -26,6 +28,13 @@ public class TestSudokuVerifier {
 	public void testVerifyStringLength_correctSolution_returnTrue(){
 		
 		assertEquals (true, verifier.verifyStringLength(correctSolution));
+		
+	}
+	
+	@Test
+	public void testVerifyStringLength_Solution_returnTrue(){
+		
+		assertEquals (false, verifier.verifyStringLength(incorrectSolution));
 		
 	}
 
