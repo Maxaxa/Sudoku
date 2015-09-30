a6fe5dd40c6b5dad1318c78e8c831712f5ae74ab
somename <someemail>
2015-09-30 15:49:54 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 	
 	String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String noSolution = "";
 
 	String incorrectSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
@@ -34,7 +35,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testVerifyStringLength_Solution_returnTrue(){
 		
-		assertEquals (false, verifier.verifyStringLength(incorrectSolution));
+		assertEquals (false, verifier.verifyStringLength(noSolution));
 		
 	}
 
