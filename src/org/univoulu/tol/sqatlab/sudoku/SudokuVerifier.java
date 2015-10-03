10854c129647df582d36591c1b26f83ae0a457a8
somename <someemail>
2015-10-04 02:32:56 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -84,9 +84,9 @@ public int[][] solutionToTwoDimensianalIntArray(String candidateSolution){
 				for(int count=1; count<10;count++)
 				if(subGrids[s][g] == count){
 					allNumbersCheck[count-1]=count;
-					System.out.println(Arrays.toString(allNumbersCheck));
 				}
 			}
+			System.out.println(Arrays.toString(allNumbersCheck));
 			for(int c =1; c <10;c++){
 				if(allNumbersCheck[c-1] != c ){
 					return false;
