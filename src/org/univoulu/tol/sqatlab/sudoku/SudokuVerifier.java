c26222cf37282e68ed0c9ffac4d7f8f73c8f5a4a
somename <someemail>
2015-10-04 01:33:47 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -98,7 +98,7 @@ public int[][] solutionToTwoDimensianalIntArray(String candidateSolution){
 				gridCounter++;
 			}
 		}
-		System.out.println(Arrays.toString(subGrids[3]));
+		System.out.println(Arrays.toString(subGrids[4]));
 		return subGrids;
 		
 	}
