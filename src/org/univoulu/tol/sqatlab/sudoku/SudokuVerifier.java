0c11e3b4e565abc12a996be2573fa7e966ab7449
somename <someemail>
2015-10-03 23:52:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -72,7 +72,7 @@ public class SudokuVerifier {
 		int [][] subGrids = new int[9][9];
 		int[] singleGrid = new int [9];
 		for(int s = 0; s < 9; s++){
-			for(int j = 0+(s*3); j<3*(s+1);j++){
+			for(int j = 0; j<3;j++){
 				for(int i = 0+(j*3)%singleGrid.length; i<(singleGrid.length/3)*((j%3)+1);i++){
 				
 					singleGrid[i] = digits[i+(singleGrid.length-3)*j];
