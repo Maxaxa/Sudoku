0da81fe545120c63bee4e6671381052bb4803391
somename <someemail>
2015-10-03 21:32:45 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,7 +2,7 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
-
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -69,7 +69,8 @@ public class TestSudokuVerifier {
 	}
 	@Test
 	public void getFirstSubGrid_gridCeck_return123123123(){
-		assertEquals ("123123123", verifier.getFirstGrid(gridCheck.toString()));
-		System.out.println(verifier.getFirstGrid(gridCheck));
+	
+		assertEquals ("[1, 3, 5, 7, 9, 2, 4, 6, 8]", verifier.getFirstGrid(gridCheck.toString()));
+		System.out.println(Array.toString(verifier.getFirstGrid(gridCheck)));
 	}
 }
