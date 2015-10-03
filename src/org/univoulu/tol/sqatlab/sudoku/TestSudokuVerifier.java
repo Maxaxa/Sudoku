f329028e43f3650041ca4d577a63cbf9edd46917
somename <someemail>
2015-10-03 23:16:42 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -70,7 +70,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void getSubGrid_gridCeck_subGridTopLeft_return123123123(){
 	int[][] subGrids= verifier.getSubGrids(gridCheck);
-		assertEquals ("[1, 2, 3, 1, 2, 3, 1, 2, 3]", Arrays.toString(subGrids[1]));
+		assertEquals ("[1, 2, 3, 1, 2, 3, 1, 2, 3]", Arrays.toString(subGrids[0]));
 		
 	}
 	@Test
@@ -81,7 +81,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void getSubGrid_gridCeck_subGridBottomRight_return789789789(){
 	int[][] subGrids= verifier.getSubGrids(gridCheck);
-		assertEquals ("[7, 8, 9, 7, 8, 9, 7, 8, 9]", Arrays.toString(subGrids[9]));
+		assertEquals ("[7, 8, 9, 7, 8, 9, 7, 8, 9]", Arrays.toString(subGrids[8]));
 		
 	}
 }
